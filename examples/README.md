656c0d0502b972461c57f0944ecace3478fb6611
Dakshinamurthy Karra <dakshinamurthy.karra@jaliansystems.com>
2021-04-30 15:40:36 +0530

    Initial Commit

---
+++ examples/README.md
@@ -0,0 +1,9 @@
+# CSV Import Example
+
+The file `quotes_dataset-partial.csv` is from https://github.com/ShivaliGoel/Quotes-500K
+
+The original file contains around 500K quotes and takes sometime to load. You can use the partial CSV file for testing.
+
+# The theme
+
+Use [quotesin](https://wordpress.org/themes/quotesin/) theme to get a nice view of the quotes imported using these files.
---
+++ wp-cli-csv-commands.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Plugin Name: WP CLI Extension for Loading CSV data
+ * Plugin URI: https://github.com/kdmurthy/wp-cli-csv-commands
+ * Description: Import CSV Data to WordPress
+ * Author: Dakshinamurthy Karra
+ * Author URI: https://github.com/kdmurthy/wp-cli-csv-commands
+ * Version: 0.1.0
+ * Text Domain: wp-cli-csv-commands
+ * Requires at least: 4.7.0
+ * Tested up to: 4.7.1
+ *
+ * @package extensions
+ */
+
+/**
+ * Load extension if WP CLI exists.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function wp_cli_csv_commands_load() {
+
+	if ( defined( 'WP_CLI' ) && WP_CLI ) {
+		include dirname( __FILE__ ) . '/class-csvcommands.php';
+	}
+
+}
+if ( function_exists( 'add_action' ) ) {
+	add_action( 'plugins_loaded', 'wp_cli_csv_commands_load' );
+} else {
+	wp_cli_csv_commands_load();
+}
